a0f2d96a328f3ea489649317f724cdbe3e2dcdb3
Juozas Krusna <43629604+Juozas1979Krusna@users.noreply.github.com>
2020-10-24 02:13:34 +0300

    Add files via upload

---
+++ Book02/Book02_P027/info-col.php
@@ -0,0 +1,8 @@
+<style>
+	#info-col{position:absolute; top:190px; right:10px; color:navy; width:135px; text-align:center; margin:5px 5px 0 0;
+}
+</style>
+<div id="info-col">
+	</h3>This is information column</h3>
+	<p>Web design by<br>A W West</p><p>&nbsp;</p>
+</div>
